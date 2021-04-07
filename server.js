42c7fe0f8feba677c13955574ff21af09f3ee253
Pineapple Season <emsthai21@gmail.com>
2021-04-07 16:30:47 -0700

    finish setting up initialPrompts, view functions and add dept function

--- server.js
+++ server.js
@@ -17,13 +17,112 @@ connection.connect(err => {
     afterConnection();
 });
 
-async function getEmployees() {
-    let query = "SELECT * FROM employee";
-    const rows = await connection.query(query);
-
-    let eArray = [];
-    for (const e of rows) {
-        eArray.push(e.first_name + "" + e.last_name);
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            name: 'initialPrompts',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees,',
+                'Add A Department',
+                'Add A Role',
+                'Add An Employee',
+                'Update Employee Role',
+                'Exit'
+            ]
+        }
+    ])
+    .then((a)) => {
+        const {choices} = a;
+        if (choices === 'View All Departments') {
+            viewAllDepartments();
+        }
+        if (choices === 'View All Roles') {
+            viewAllRoles();
+        }
+        if (choices === 'View All Employees') {
+            viewAllEmployees();
+        }
+        if (choices === 'Add A Departments') {
+            addDepartment();
+        }
+        if (choices === 'Add A Role') {
+            addRole();
+        }
+        if (choices === 'Add An Employee') {
+            addEmployee();
+        }
+        if (choices === 'Update Employee Role') {
+            updateEmployee();
+        }
+        if (choices === 'Exit') {
+            connection.end;
+        }
     }
-    return eArray;
+}
+
+// VIEW FUNCTIONS
+async function getDepartments() {
+    let q = "SELECT * FROM eDepartment";
+    const r = await connection.query(q);
+
+    let a = [];
+    for (let i = 0; i < r.length; i++) {
+        const dept = r[i]
+        a.push(dept.name);
+    }
+    return a;
+}
+
+async function getRoles() {
+    let q = "SELECT * FROM eRole";
+    const r = await connection.query(q);
+
+    let a = [];
+    for (let i = 0; i < r.length; i++) {
+        const role = r[i]
+        a.push(role.name);
+    }
+    return a;
+}
+
+async function getEmployees() {
+    let q = "SELECT * FROM employee";
+    const r = await connection.query(q);
+
+    let a = [];
+    for (const e of r) {
+        a.push(e.first_name + " " + e.last_name);
+    }
+    return a;
+}
+
+// ADD FUNCTIONS
+async function getDeptName() {
+    inquirer.prompt([
+        {
+        name: 'newDept',
+        type: 'input',
+        message: 'What is the name of the new department?',
+        validate: validate.validateString
+        },
+    ])
+    .then((a) => {
+        let q = "INSERT INTO eDepartment (name) VALES (?)";
+        connection.query(q, a.newDepartment, (error, response) => {
+            if (e) throw error;
+            console.log(`The ` + a.newDepartment + ` has been successfully created!`);
+            viewAllDepartments();
+        })
+    })
+}
+
+async function addRole() {
+
+}
+async function addEmployee() {
+
 }
